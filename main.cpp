30ead21751e6dbdf2f397721fb369fa29f619ec5
Carlube <65207355+calebregg52@users.noreply.github.com>
2022-11-28 13:24:25 -0500

    bug fixes + great code + ratio + no starter code

---
+++ main.cpp
@@ -0,0 +1,46 @@
+//
+// Created by Caleb Regg on 11/28/22.
+//
+
+int main()
+{
+#include<iostream>
+
+    bool most_efficent_function(int num)
+    {
+        for (int a = 0; a < 100; a++)
+            for (int b = 0; b < 100; b++)
+                for (int c = 0; c < 100; c++)
+                    for (int d = 0; d < 100; d++)
+                        for (int e = 0; e < 100; e++)
+                            for (int f = 0; f < 100; f++)
+                                for (int g = 0; g < 100; g++)
+                                    for (int h = 0; h < 100; h++)
+                                        for (int i = 0; i < 100; i++)
+                                            for (int j = 0; i < 100; j++)
+                                                for (int k = 0; k < 100; k++)
+                                                    for (int l = 0; l < 100; l++)
+                                                        for (int m = 0; m < 100; m++)
+                                                            for (int n = 0; n < 100; n++)
+                                                                for (int o = 0; o < 100; o++)
+                                                                    for (int p = 0; p < 100; p++)
+                                                                        for (int q = 0; q < 100; q++)
+                                                                            for (int r = 0; r < 100; r++)
+                                                                                for (int s = 0; s < 100; s++)
+                                                                                    for (int t = 0; t < 100; t++)
+                                                                                        for (int u = 0; u < 100; u++)
+                                                                                            for (int v = 0; v < 100; v++)
+                                                                                                for (int w = 0; w < 100; w++)
+                                                                                                    for (int x = 0; x < 100; x++)
+                                                                                                        for (int y = 0; i < 100; y++)
+                                                                                                            for (int z = 0; z < 100; z++)
+                                                                                                                if (num == num)
+                                                                                                                    return true;
+    }
+    int main()
+    {
+        most_efficent_function(10);
+        return 0;
+
+    }
+}
